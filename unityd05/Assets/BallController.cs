3a87e6d41df0a244ad95dc84517d5e197a0282d1
Denis MOUREU-LARRANG <dmoureu-@e1r11p2.42.fr>
2017-06-19 23:37:57 +0200

    ok

---
+++ unityd05/Assets/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+
+	public float speedH = 2.0f;
+	public float speedV = 2.0f;
+
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.transform.position = new Vector3 (1, 10,-20) + this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKey (KeyCode.Space))
+			this.GetComponent<Rigidbody> ().AddForce (new Vector3 (10, 0, 0));
+
+		if (Input.GetKey (KeyCode.UpArrow))
+			this.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 0, 10));
+
+		if (Input.GetKey (KeyCode.DownArrow))
+			this.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 0, -10));
+
+		if (Input.GetKey (KeyCode.RightArrow))
+			this.GetComponent<Rigidbody> ().AddForce (new Vector3 (10, 0, 0));
+		if (Input.GetKey (KeyCode.LeftArrow))
+			this.GetComponent<Rigidbody> ().AddForce (new Vector3 (-10, 0, 0));
+
+
+		if (Input.GetKey (KeyCode.E))
+			Camera.main.transform.position += new Vector3 (0,1,0);
+		if (Input.GetKey (KeyCode.Q))
+			Camera.main.transform.position += new Vector3 (0,-1,0);
+
+
+			yaw += speedH * Input.GetAxis("Mouse X");
+			pitch -= speedV * Input.GetAxis("Mouse Y");
+
+			Camera.main.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+		//Camera.main.transform.position = new Vector3 (1, 10,-20) + this.transform.position;
+	}
+}
---
+++ unityd05/Assets/CameraController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	float flySpeed = 10F;
+	GameObject defaultCam;
+	GameObject playerObject;
+	bool isEnabled;
+
+	bool shift;
+	bool ctrl;
+	float accelerationAmount = 30;
+	float accelerationRatio = 3;
+	float slowDownRatio = 0.2F;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		//use shift to speed up flight
+		if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+		{
+			shift = true;
+			flySpeed *= accelerationRatio;
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+		{
+			shift = false;
+			flySpeed /= accelerationRatio;
+		}
+
+		//use ctrl to slow up flight
+		if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+		{
+			ctrl = true;
+			flySpeed *= slowDownRatio;
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
+		{
+			ctrl = false;
+			flySpeed /= slowDownRatio;
+		}
+		//
+		if (Input.GetAxis("Vertical") != 0)
+		{
+			//transform.Translate(Vector3.forward * flySpeed * Input.GetAxis("Vertical"));
+			this.GetComponent<Rigidbody> ().AddForce (Vector3.forward * flySpeed * Input.GetAxis ("Vertical"));
+		}
+
+
+		if (Input.GetAxis("Horizontal") != 0)
+		{
+			//transform.Translate(Vector3.right * flySpeed * Input.GetAxis("Horizontal"));
+			this.GetComponent<Rigidbody> ().AddForce (Vector3.right * flySpeed * Input.GetAxis("Horizontal"));
+		}
+
+
+		if (Input.GetKey(KeyCode.E))
+		{
+			this.GetComponent<Rigidbody> ().AddForce (Vector3.up * flySpeed);
+		}
+		else if (Input.GetKey(KeyCode.Q))
+		{
+			transform.Translate(Vector3.down * flySpeed);
+		}
+		//if (Input.GetKeyDown(KeyCode.F12))
+		//switchCamera();
+
+		if (Input.GetKeyDown(KeyCode.M))
+			playerObject.transform.position = transform.position; //Moves the player to the flycam's position. Make sure not to just move the player's camera.
+
+	}
+}
